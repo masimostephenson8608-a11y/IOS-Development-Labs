2fa615d6391e7a98e4de3ff2d3ef9bdaf2aa8b4e
Masimo Stephenson <masimo.stephenson8608@stu.mtec.edu>
2025-10-07 21:48:55 -0600

    Fixed the text fields to display that car structs set value for integers instead of 0

--- ListLab/ListLab/DetailsView.swift
+++ ListLab/ListLab/DetailsView.swift
@@ -13,14 +13,21 @@ struct DetailsView: View {
     @State private var modelChange: String = ""
     @State private var milesChange: Int = 0
     @State private var yearChange: Int = 0
+    
+    let someFormat: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .none
+        formatter.zeroSymbol = ""
+        return formatter
+    }()
     var body: some View {
         Form {
             Section(header: Text("Car Information")) {
                 TextField("\(carDetail.make)", text: $makeChange)
                 TextField("\(carDetail.model)", text: $modelChange)
-                TextField("\(carDetail.miles, format: .number)", value: $milesChange, format: .number )
+                TextField("\(carDetail.miles, format: .number)", value: $milesChange, formatter: someFormat )
                     .keyboardType(.numberPad)
-                TextField("\(carDetail.year, format: .number)", value: $yearChange, format: .number)
+                TextField("\(carDetail.year, format: .number)", value: $yearChange, formatter: someFormat)
                     .keyboardType(.numberPad)
             }
         }
