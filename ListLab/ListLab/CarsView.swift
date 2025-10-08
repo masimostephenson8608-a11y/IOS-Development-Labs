899a467e8f1319c2b97c37433ed898a8118417e0
Masimo Stephenson <masimo.stephenson8608@stu.mtec.edu>
2025-10-08 10:13:13 -0600

    Cleaned up the lists lab

--- ListLab/ListLab/CarsView.swift
+++ ListLab/ListLab/CarsView.swift
@@ -15,7 +15,7 @@ struct Car: Identifiable {
     var year: Int
     
     mutating func submit(make: String, model: String,
-                miles: Int, year: Int) {
+                         miles: Int, year: Int) {
         self.make = make
         self.miles = miles
         self.model = model
@@ -33,7 +33,7 @@ struct CarsView: View {
         Car(make: "Hyundai", model: "Elantra", miles: 15000, year: 2022)
     ]
     
-//    let carCount = cars.count
+    //    let carCount = cars.count
     var body: some View {
         NavigationStack {
             ZStack {
@@ -43,57 +43,52 @@ struct CarsView: View {
                     startRadius: 20,
                     endRadius: 900
                 ).ignoresSafeArea()
-                
-                HStack {
-                    
-                    ScrollView {
-                        HStack {
-                            VStack {
-                                ForEach($cars) { $car in
-                                    ZStack {
-                                        RoundedRectangle(cornerRadius: 20).frame(
-                                            width: 200,
-                                            height: 200
-                                        ).foregroundStyle(
-                                            LinearGradient(
-                                                colors: [.blue, .black],
-                                                startPoint: .trailing,
-                                                endPoint: .leading
-                                            )
+                ScrollView {
+                    VStack {
+                        ForEach($cars) { $car in
+                                VStack {
+                                    Text("\(car.make) \(car.model)")
+                                        .font(
+                                            .headline.weight(.heavy)
+                                            .pointSize(20)
                                         )
-                                        VStack {
-                                            Text("\(car.make) \(car.model)")
-                                                .font(
-                                                    .headline.weight(.heavy)
-                                                    .pointSize(20)
-                                                )
-                                                .foregroundStyle(.white)
-                                            
-                                            NavigationLink(
-                                                "Click for more details",
-                                                destination: DetailsView(carDetail: $car)
-                                            )
-                                            .fontDesign(.serif).foregroundStyle(
-                                                .white.secondary
-                                            )
-                                            .offset(y: 20)
-                                            
-                                        }
-                                    }
+                                        .foregroundStyle(.white)
+                                        .padding(12)
+                                    
+                                    NavigationLink(
+                                        "Click for more details",
+                                        destination: DetailsView(carDetail: $car)
+                                    )
+                                    .padding(.bottom, 12)
+                                    
+                                    .fontDesign(.serif).foregroundStyle(
+                                        .white.secondary
+                                    )
                                 }
-                            }
-                            Spacer()
+                                .padding(12)
+                                .frame(maxWidth: .infinity)
+                                .background {
+                                    RoundedRectangle(cornerRadius: 20).foregroundStyle(
+                                        LinearGradient(
+                                            colors: [.blue, .black],
+                                            startPoint: .trailing,
+                                            endPoint: .leading
+                                        )
+                                    )
+                                }
                         }
-                        .padding()
                     }
-                    VStack(alignment: .leading) {
-                        NavigationLink {
-                            NewCarView(carList: $cars)
-                        } label: {
-                            Image(systemName: "plus").frame(width: 40, height: 40).foregroundStyle(.white).glassEffect()
-                        }
-                        Spacer()
-                    }.padding(.horizontal, 50)
+                    Spacer()
+                }
+                .padding()
+            }
+            .toolbar {
+                ToolbarItem {
+                    NavigationLink {
+                        NewCarView(carList: $cars)
+                    } label: {
+                        Image(systemName: "plus").frame(width: 40, height: 40).foregroundStyle(.white)
+                    }
                 }
             }
         }
