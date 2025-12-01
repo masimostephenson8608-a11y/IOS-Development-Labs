3e704049a54d4cbdf53345916e064af46dae7ea6
Masimo Stephenson <masimo.stephenson8608@stu.mtec.edu>
2025-12-01 09:54:19 -0700

    Checked off everything

--- Projects/Starter - Concurrency (iTunes Search Pt 3)/iTunesSearch/iTunesSearch/StoreItemListView.swift
+++ Projects/Starter - Concurrency (iTunes Search Pt 3)/iTunesSearch/iTunesSearch/StoreItemListView.swift
@@ -7,64 +7,6 @@
 
 import SwiftUI
 
-@Observable
-class StoreItemListViewModel {
-    var items: [StoreItem] = []
-    private var storeItemController = StoreItemController()
-    var previewTask: Task<Void, Never>? = nil
-    var previewData: Data? = nil
-    
-    var searchText = ""
-    var selectedMediaType: MediaType = .music
-
-    func fetchMatchingItems() async -> [StoreItem] {
-        var results: [StoreItem] = []
-            if !searchText.isEmpty {
-                do {
-                    results = try await storeItemController.fetchItems(searchText: searchText, mediaType: selectedMediaType)
-                    return results
-                } catch {
-                    print("Failed inside viewModel object")
-                    return []
-                }
-            
-            // set up query dictionary
-            
-            // use the item controller to fetch items
-            // if successful, use the main queue to set self.items
-            // otherwise, print an error to the console
-            } else {
-                return []
-            }
-    }
-    
-    func fetchPreview(item: StoreItem) {
-        if let previewTask {
-                previewTask.cancel()
-            }
-        
-        self.previewData = nil
-        
-        guard let string = item.previewUrl, let url = URL(string: string) else {
-                    return
-                }
-
-            previewTask = Task {
-                // Code to fetch the preview data using the URL
-                do {
-                    let data = try await storeItemController.fetchPreview(from: url)
-                    
-                    self.previewData = previewData
-                } catch {
-                    if Task.isCancelled { return }
-                    print("Failed to fetch preview: \(error)")
-                }
-                        
-                // Once the task is complete, return the stored task value to nil
-                previewTask = nil
-            }
-    }
-}
 
 struct StoreItemListView: View {
     @State private var viewModel = StoreItemListViewModel()
@@ -95,9 +37,7 @@ struct StoreItemListView: View {
                 
                 List(viewModel.items) { item in
                     ItemCellView(item: item) {
-                        Task {
-                            await viewModel.fetchPreview(item: item)
-                        }
+                        viewModel.fetchPreview(item: item)
                     }
                 }
                 .listStyle(.plain)
--- Projects/Starter - Concurrency (iTunes Search Pt 3)/iTunesSearch/iTunesSearch/iTunesSearchApp.swift
+++ Projects/Starter - Concurrency (iTunes Search Pt 3)/iTunesSearch/iTunesSearch/iTunesSearchApp.swift
@@ -8,51 +8,6 @@
 import SwiftUI
 import Foundation
 
-import CoreHaptics
-
-struct ItunesSearchView: View {
-    
-    
-    var body: some View {
-        Text("Searching Itunes...")
-            .onAppear() {
-                Task {
-//                    await buildURL()
-                }
-            }
-    }
-//    func buildURL() async {
-//        let baseURLString = "https://itunes.apple.com/search"
-//        
-//        var components = URLComponents(string: baseURLString)
-//        
-//        let queryItems = [
-//            URLQueryItem(name: "term", value: "Jack Johnson"),
-//            URLQueryItem(name: "limit", value: "10")
-//        ]
-//        
-//        components?.queryItems = queryItems
-//        
-//        Task {
-//            do {
-//                guard let url = components?.url else {
-//                    print("Inavlid URL")
-//                    return
-//                }
-//                let (data, response) = try await URLSession.shared.data(from: url)
-//                
-//                if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200,
-//                   let string = String(data: data, encoding: .utf8) {
-//                    print(string)
-//                }
-//            } catch {
-//                print("Failed The Task Block")
-//            }
-//        }
-//    }
-}
-
-
 @main
 struct iTunesSearchApp: App {
     var body: some Scene {
