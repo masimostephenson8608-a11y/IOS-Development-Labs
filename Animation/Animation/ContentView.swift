aa6fd5cc987f3c68e0cd720c87947f7b4975db63
Masimo Stephenson <masimo.stephenson8608@stu.mtec.edu>
2025-12-10 08:29:07 -0700

    Still working on the Animation lab but starting the Game Tracker Project

---
+++ Animation/Animation/ContentView.swift
@@ -0,0 +1,68 @@
+//
+//  ContentView.swift
+//  Animation
+//
+//  Created by Masimo Stephenson on 12/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var countDown = 3
+    @State var scaleEffect = 1.0
+    @State var opacity = 0.0
+    @State var showGo = false
+    @State private var gameStarted = false
+    
+    var body: some View {
+        VStack {
+            if gameStarted == true {
+                Text("\(countDown)")
+                    .font(.system(size: 500))
+                    .scaleEffect(scaleEffect)
+                    .opacity(opacity)
+                    .onAppear() {
+                        animateNumber()
+                    }
+            } else if showGo == true {
+                Text("GO!")
+                    .font(.system(size: 200))
+            }
+            Spacer()
+            Button("Start Countdown") {
+                gameStarted = true
+            }
+        }
+        .padding()
+    }
+    
+    func animateNumber() {
+        switch countDown {
+        case 1...3:
+            withAnimation(.easeIn(duration: 1)) {
+                scaleEffect = 0.4
+                opacity = 1
+            }
+            
+            
+        case 0:
+            gameStarted = false
+            showGo = true
+            
+        default:
+            gameStarted = false
+        }
+            
+            Task {
+                try? await Task.sleep(for: .milliseconds(1000))
+                scaleEffect = 1
+                opacity = 0
+                countDown -= 1
+                animateNumber()
+            }
+        }
+    }
+
+#Preview {
+    ContentView()
+}
